5f835012e7794628bad1498eb367c6735958a232
Nada-235 <nada.alshakiry@gmail.com>
2023-07-24 20:36:10 +0300

    Coded Student list

--- src/component/StudentsList.jsx
+++ src/component/StudentsList.jsx
@@ -5,21 +5,21 @@ import '../StudentsList.css';
 export default function StudentsList() {
   const students = ['Rusul', 'Sara', 'Yasmin', 'Mais'];
 
-  const ShoutForStudent = (student) => {
+  const shoutForStudent = (student) => {
     alert(`${student} is here`);
   };
 
-  const StudentList = students.map((student) => (
+  const studentList = students.map((student) => (
   <ul>
     <li key={student}>
-      <p> {student} </p> <button onClick={() => ShoutForStudent(student)}> Call {student} </button>
+      <p> {student} </p> <button onClick={() => shoutForStudent(student)}> Call {student} </button>
     </li>
   </ul>));
 
   return (
     <div>
       <h1> Students List </h1>
-      {StudentList}
+      {studentList}
 
       {/* <ul>
          {students.map((student) => (
